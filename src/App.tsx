8084f3204d0fa077e8cf566b13ab8e4342aa9f3d
oyazicioglu <oyazicioglu@hotmail.com>
2023-02-18 02:25:57 +0300

    Add Project edit route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { Route, Routes } from 'react-router';
+import { Route, Router, Routes } from 'react-router';
 import { MemoryRouter } from 'react-router-dom';
 import { DefaultLayout } from './layouts/DefaultLayout';
 import { Home } from './views/home/Home';
+import { Project } from './views/projects/project/Project';
 import { Projects } from './views/projects/Projects';
 
 const container = document.getElementById('app');
@@ -13,6 +14,7 @@ root.render(
             <Route element={<DefaultLayout></DefaultLayout>}>
                 <Route element={<Home></Home>} path="/"></Route>
                 <Route element={<Projects></Projects>} path="/projects"></Route>
+                <Route element={<Project></Project>} path="/projects/edit/:id"></Route>
             </Route>
         </Routes>
     </MemoryRouter>
