69f673ad571b981035b05a8e6c11c72e5acd5a7d
oyazicioglu <oyazicioglu@hotmail.com>
2023-02-18 02:24:46 +0300

    Change Prisma structure

---
+++ prisma/migrations/20230217230300_initi/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Project` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `active` BOOLEAN NULL DEFAULT false,
+    `updatedAt` DATETIME(3) NULL,
+    `createdAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
